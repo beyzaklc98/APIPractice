2b46b88a32e5469149e44c5540f4f0ff8e2a9cfb
Beyza Kılıç <beyza7259@gmail.com>
2022-11-09 22:24:11 +0300

    APIPractice

--- src/test/java/pojos/Country.java
+++ src/test/java/pojos/Country.java
@@ -1,55 +1,56 @@
 package pojos;
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Country {
     /*
-    “country”: {
-            “id”: 3,
-            “name”: “USA”,
-            “states”: null
-        },
+    "country": {
+        "id": 3,
+        "name": "USA",
+        "states": null
+    }
+     */
+    /*
+    <dependency>
+    <groupId>org.projectlombok</groupId>
+    <artifactId>lombok</artifactId>
+    <version>RELEASE</version>
+    <scope>test</scope>
+</dependency>
      */
     private Integer id;
     private String name;
-    private String states;
-
-    public Country(Integer id, String name, String states) {
+    private Object states;
+    public Country() {
+    }
+    public Country(Integer id, String name, Object states) {
         this.id = id;
         this.name = name;
         this.states = states;
     }
-
-    public Country() {
-    }
-
     public Integer getId() {
         return id;
     }
-
     public void setId(Integer id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
-    public String getStates() {
+    public Object getStates() {
         return states;
     }
-
-    public void setStates(String states) {
+    public void setStates(Object states) {
         this.states = states;
     }
-
     @Override
     public String toString() {
         return "Country{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
-                ", states='" + states + '\'' +
+                ", states=" + states +
                 '}';
     }
 }
--- src/test/java/pojos/Customer.java
+++ src/test/java/pojos/Customer.java
@@ -1,23 +1,59 @@
 package pojos;
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Customer {
-    private Integer id;
-    private String firstName;
-    private String lastName;
-    private String middleInitial;
-    private String email;
-    private String mobilePhoneNumber;
-    private String phoneNumber;
-    private String zipCode;
-    private String address;
-    private String city;
-    private Integer ssn;
-    private String createDate;
-    private Boolean zelleEnrolled;
-    private Country country;
+    /*
+       {
+       “id”: 110452,
+       “firstName”: “Jasmine”,
+       “lastName”: “Stehr”,
+       “middleInitial”: “V”,
+       “email”: “marni.zboncak@yahoo.com”,
+       “mobilePhoneNumber”: “463-609-2097”,
+       “phoneNumber”: “1-112-497-0270”,
+       “zipCode”: “16525”,
+       “address”: “14387 Al Ridge5343 Bert Burgs”,
+       “city”: “Waltermouth”,
+       “ssn”: “761-59-2911”,
+       “createDate”: “2021-11-28T21:00:00Z”,
+       “zelleEnrolled”: false,
+       “country”: {
+           “id”: 3,
+           “name”: “USA”,
+           “states”: null
+       },
+       “state”: “California”,
+       “user”: {
+           “id”: 110016,
+           “login”: “leopoldo.reinger”,
+           “firstName”: “Jasmine”,
+           “lastName”: “Stehr”,
+           “email”: “marni.zboncak@yahoo.com”,
+           “activated”: true,
+           “langKey”: “en”,
+           “imageUrl”: null,
+           “resetDate”: null
+       },
+       “accounts”: []
+   }
+        */
+    private  Integer id;
+    private  String firstName;
+    private  String lastName;
+    private  String middleInitial;
+    private  String email;
+    private  String mobilePhoneNumber;
+    private  String phoneNumber;
+    private  String zipCode;
+    private  String address;
+    private  String city;
+    private  String ssn;
+    private  String createDate;
+    private  Boolean zelleEnrolled;
+    private  Country country;
     private String state;
     private User user;
-    private String accounts;
-    public Customer(Integer id, String firstName, String lastName, String middleInitial, String email, String mobilePhoneNumber, String phoneNumber, String zipCode, String address, String city, Integer ssn, String createDate, Boolean zelleEnrolled, Country country, String state, User user, String accounts) {
+    public Customer(Integer id, String firstName, String lastName, String middleInitial, String email, String mobilePhoneNumber, String phoneNumber, String zipCode, String address, String city, String ssn, String createDate, Boolean zelleEnrolled, Country country, String state, User user) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
@@ -34,7 +70,6 @@ public class Customer {
         this.country = country;
         this.state = state;
         this.user = user;
-        this.accounts = accounts;
     }
     public Customer() {
     }
@@ -98,10 +133,10 @@ public class Customer {
     public void setCity(String city) {
         this.city = city;
     }
-    public Integer getSsn() {
+    public String getSsn() {
         return ssn;
     }
-    public void setSsn(Integer ssn) {
+    public void setSsn(String ssn) {
         this.ssn = ssn;
     }
     public String getCreateDate() {
@@ -134,12 +169,6 @@ public class Customer {
     public void setUser(User user) {
         this.user = user;
     }
-    public String getAccounts() {
-        return accounts;
-    }
-    public void setAccounts(String accounts) {
-        this.accounts = accounts;
-    }
     @Override
     public String toString() {
         return "Customer{" +
@@ -153,13 +182,12 @@ public class Customer {
                 ", zipCode='" + zipCode + '\'' +
                 ", address='" + address + '\'' +
                 ", city='" + city + '\'' +
-                ", ssn=" + ssn +
+                ", ssn='" + ssn + '\'' +
                 ", createDate='" + createDate + '\'' +
                 ", zelleEnrolled=" + zelleEnrolled +
                 ", country=" + country +
                 ", state='" + state + '\'' +
                 ", user=" + user +
-                ", accounts='" + accounts + '\'' +
                 '}';
     }
 }
